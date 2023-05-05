5129dbe5801e1a1ff5d29627816d7b2b17fba2f6
thecomet28 <47154526+thecomet28@users.noreply.github.com>
2023-05-05 12:03:28 -0400

    Update update-nvts.sh
    Removed while loop for cron to handle task

--- scripts/update-nvts.sh
+++ scripts/update-nvts.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# This script update the NVTs in the background every 12 hours.
-set -Eeuo pipefail
 
-while true; do
-	echo "Running Automatic NVT update..."
-	su -c "greenbone-feed-sync -v --compression-level=9 --type=nvt" openvas-sync
-	su -c "greenbone-feed-sync -v --compression-level=9 --type=notus" openvas-sync
-	sleep 43200
-done
+echo "Running Automatic NVT update..."
+su -c "greenbone-feed-sync -v --compression-level=9 --type=nvt" openvas-sync
+sleep 5
+su -c "greenbone-feed-sync -v --compression-level=9 --type=notus" openvas-sync
